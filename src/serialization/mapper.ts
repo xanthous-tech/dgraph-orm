3ac0f69b8b6029a62eecc7605c1630a587f2253b
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2019-11-28 20:01:28 +0800

    addResourceMethod to Objectmapper

--- src/serialization/mapper.ts
+++ src/serialization/mapper.ts
@@ -1,13 +1,14 @@
-import { plainToClass } from 'class-transformer';
+import {plainToClass} from 'class-transformer';
 
-import { ObjectLiteral } from '../utils/type';
-import { Constructor } from '../utils/class';
-import { DiffTracker } from '../mutation/tracker';
+import {ObjectLiteral} from '../utils/type';
+import {Constructor} from '../utils/class';
+import {DiffTracker} from '../mutation/tracker';
 
 export namespace ObjectMapper {
   class ObjectMapperBuilder<T = any> {
     private _entryType: Constructor<T>;
     private _jsonData: ObjectLiteral<any>;
+    private _resource: Map<string, ObjectLiteral<any>> = new Map<string, ObjectLiteral<any>>();
 
     addEntryType(type: Constructor<T>) {
       this._entryType = type;
@@ -19,7 +20,23 @@ export namespace ObjectMapper {
       return this;
     }
 
+    addResourceData(data: ObjectLiteral<any> | ObjectLiteral<any>[]) {
+      if (data instanceof Array) {
+        data.forEach(d => {
+          if (d.uid) {
+            this._resource.set(d.uid, d)
+          }
+        });
+      } else {
+        if (data.uid) {
+          this._resource.set(data.uid, data)
+        }
+      }
+      return this;
+    }
+
     build(): T[] {
+      this.expand(this._jsonData);
       const instance: T | T[] = plainToClass(this._entryType as any, this._jsonData);
       if (Array.isArray(instance)) {
         instance.forEach(i => DiffTracker.purgeInstance(i));
@@ -28,6 +45,17 @@ export namespace ObjectMapper {
 
       DiffTracker.purgeInstance(instance);
       return [instance];
+    }
+
+    private expand(source: ObjectLiteral) {
+      if (this._resource.has(source.uid)) {
+        source = {...source, ...this._resource.get((source.uid))}
+      }
+      Object.keys(source).map(key => {
+        if (source[key] instanceof Array) {
+          this.expand(source[key])
+        }
+      });
     }
   }
 
