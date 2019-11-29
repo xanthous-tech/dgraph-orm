b7f390e328e0e0d6b02f241d9c147eb33a7c0330
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2019-11-29 22:16:29 +0800

    convert expand to modify data in place

--- src/serialization/mapper.ts
+++ src/serialization/mapper.ts
@@ -41,7 +41,7 @@ export namespace ObjectMapper {
       // resource data.
       if (this._resource.size > 0) {
         const visited = new Set<string>();
-        this._jsonData = Array.isArray(this._jsonData)
+        Array.isArray(this._jsonData)
           ? this._jsonData.map(jd => Private.expand(visited, this._resource, jd))
           : Private.expand(visited, this._resource, this._jsonData);
       }
@@ -68,30 +68,30 @@ export namespace ObjectMapper {
  */
 namespace Private {
   /**
-   * Walk down a tree recursively, matching node uid in the resource data and adding extra information.
+   * Visit all nodes in a tree recursively, matching node uid in the resource data and adding extra information.
+   *
+   * ### NOTE
+   * Expand will modify the data in-place.
    */
   export function expand(visited: Set<string>, resource: ObjectLiteral<any>, source: ObjectLiteral<any>) {
     if (resource.has(source.uid)) {
-      source = { ...source, ...resource.get(source.uid) };
+      Object.assign(source, resource.get(source.uid));
     }
 
     Object.keys(source).forEach(key => {
-      if (Array.isArray(source[key])) {
-        source[key] = source[key].map((node: any) => {
-          const visitingKey = `${source.uid}+${key}+${node.uid}`;
-
-          if (visited.has(visitingKey)) {
-            // TODO: Check if this appears in the
-            //  data after build.
-            return 'CIRCULAR_STOP';
-          }
-
-          visited.add(visitingKey);
-          return expand(visited, resource, node);
-        });
+      if (!Array.isArray(source[key])) {
+        return;
       }
-    });
 
-    return source;
+      source[key].forEach((node: any) => {
+        const visitingKey = `${source.uid}:${key}:${node.uid}`;
+        if (visited.has(visitingKey)) {
+          return;
+        }
+
+        visited.add(visitingKey);
+        return expand(visited, resource, node);
+      });
+    });
   }
 }
