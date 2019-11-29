3cae423e4ecaed3c359a253f37652ba9917c3346
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2019-11-29 21:42:07 +0800

    tidy up toObject

--- src/utils/public.ts
+++ src/utils/public.ts
@@ -21,23 +21,20 @@ export namespace Utils {
     );
 
     // Drill down to the predicates if metadata found in the storage.
-    const predicatesMetadata = MetadataStorage.Instance.predicates.get(instance.constructor.name);
-    predicatesMetadata &&
-      predicatesMetadata.forEach(p => {
-        const predicateNodes: Predicate<any> = Reflect.get(instance, p.args.propertyName);
-        if (predicateNodes) {
-          if (Array.isArray(predicateNodes.get())) {
-            // Convert predicates to plain object.
-            Object.defineProperty(object, p.args.propertyName, {
-              enumerable: true,
-              value: predicateNodes.get().reduce((acc, pn) => {
-                acc.push(Utils.toObject(pn));
-                return acc;
-              }, [])
-            });
-          }
-        }
-      });
+    const predicatesMetadata = MetadataStorage.Instance.predicates.get(instance.constructor.name) || [];
+    predicatesMetadata.forEach(p => {
+      const predicateNodes: Predicate<any> = Reflect.get(instance, p.args.propertyName);
+      if (predicateNodes && Array.isArray(predicateNodes.get())) {
+        // Convert predicates to plain object.
+        Object.defineProperty(object, p.args.propertyName, {
+          enumerable: true,
+          value: predicateNodes.get().reduce((acc, pn) => {
+            acc.push(Utils.toObject(pn));
+            return acc;
+          }, [])
+        });
+      }
+    });
 
     return object;
   }
